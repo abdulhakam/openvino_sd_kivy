ad66571c2b6ba99630d1aa59591491bb820dc3d9
Abdul Hakam <zehas.11@gmail.com>
2024-05-02 13:21:23 +0500

    preparing as github repo

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from conf_vars import diffusers_path, ov_path
+from conf_vars import diffusers_path, ov_path, vae_path
 
 
 ############################ DIFFUSERS TO OPENVINO ############################
@@ -24,7 +24,7 @@ def diffusers_to_ov(
     del ov_pipe
 
 
-########################### SAFETENSORS TO DIFFUSERS ###########################
+########################### SAFETENSORS TO DIFFUSERS ##########################
 def create_diffusers_from_safetensors(input_file):
     from diffusers import StableDiffusionPipeline
 
@@ -38,3 +38,20 @@ def create_diffusers_from_safetensors(input_file):
     pipe.save_pretrained(diffusers_path.as_posix())
     print("saved diffusers to " + diffusers_path.as_posix())
     del pipe
+
+
+########################### SAFETENSORS IN OPENVINO ###########################
+def load_to_ov_pipeline(
+    input_file, batch_size=1, height=512, width=512, num_images_per_prompt=1
+):
+    from diffusers import StableDiffusionPipeline
+    sd_pipe = StableDiffusionPipeline.from_single_file(
+        input_file,
+        scheduler_type="euler-ancestral",
+        load_safety_checker=False,
+        vae=vae_path)
+    sd_pipe.to('gpu')                                      ####################just playing around
+    sd_pipe.save_pretrained("./cache/tempdiffuser")
+    from optimum.intel.openvino import OVStableDiffusionPipeline
+    print("loading from" + input_file)
+    ov_pipe = OVStableDiffusionPipeline.from_pretrained("./cache/tempdiffuser",export=False,cache_dir='./cache',local_files_only=True)
